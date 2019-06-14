c475cff0883c3780932ae50e5cd3703e185f9e42
riqin wu <wu000210@algonquinlive.com>
2019-06-14 10:52:09 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button type="primary" @click="openDiag = ture">Primary</el-button>
+    <el-dialog :visible="openDiag">
+    <div>
+    <input type="text" v-model="sendTreat" />
+    </div>
+    <HelloWorld msg="My first App"/>
+    <MyFirst :treat="sendTreat"/>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import MyFirst from './components/MyFirst.vue';
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
+    MyFirst 
   },
+  data: function() {
+    return {
+      sendTreat: "Raw Hide",
+      openDiag: false
+    };
+  }  
 };
 </script>
 
---
+++ src/components/MyFirst.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+<h2>Hello{{ name}}!
+</h2>
+<ul>
+ <li>Age: {{ age }}</li>
+ <li>Color: {{ color }}</li>
+ <li>{{ treat }}</li>
+</ul>
+
+</div>
+</template>
+<script>
+export default {
+    props: ["treat"],
+    data: function() {
+        return {
+            name:"MyFirst",
+            age: 6,
+            color: "pink"
+        };
+    }
+};
+</script>
+<style>
+h2 {
+    color: coral;
+}
+</style>
